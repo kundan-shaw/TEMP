95117ba39c9678d6b69ff54eef83bfb80f315583
kundan-shaw <143525564+kundan-shaw@users.noreply.github.com>
2023-09-13 15:04:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const celsiusInput = document.getElementById("celsius");
+    const fahrenheitInput = document.getElementById("fahrenheit");
+    const convertButton = document.getElementById("convertButton");
+
+    convertButton.addEventListener("click", function () {
+        const celsius = parseFloat(celsiusInput.value);
+        if (!isNaN(celsius)) {
+            const fahrenheit = (celsius * 9/5) + 32;
+            fahrenheitInput.value = fahrenheit.toFixed(2);
+        } else {
+            alert("Please enter a valid temperature in Celsius.");
+        }
+    });
+});
